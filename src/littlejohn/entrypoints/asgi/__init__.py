0a70e63a0f440d0f65d49e8def6a4a3cd733284a
mechpig <7295856+mech-pig@users.noreply.github.com>
2021-11-11 18:51:29 +0100

    feat(portfolio): `get_current_prices` returns stocks in user portfolio

--- src/littlejohn/entrypoints/asgi/__init__.py
+++ src/littlejohn/entrypoints/asgi/__init__.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 
+from littlejohn.adapters.portfolio_repository import (
+    PortfolioRepositoryDeterministicGenerator,
+)
 from littlejohn.domain.service import StockService
 
 from . import api
 
 
 def create_app() -> FastAPI:
-    service = StockService()
+    portfolio_repository = PortfolioRepositoryDeterministicGenerator(
+        portfolios={
+            "416076429e6f437c8b7dcdbc18d608ac": ["TEST", "TEST2"],
+        }
+    )
+    service = StockService(portfolio_repository=portfolio_repository)
     return api.create(service=service)
--- tests/entrypoints/test_asgi.py
+++ tests/entrypoints/test_asgi.py
@@ -4,22 +4,33 @@ from uuid import uuid4
 import pytest
 from fastapi.testclient import TestClient
 
+from littlejohn.adapters.portfolio_repository import (
+    PortfolioRepositoryDeterministicGenerator,
+)
 from littlejohn.domain.entities import StockPrice
-from littlejohn.domain.service import StockService
+from littlejohn.domain.service import PortfolioRepository, StockService
 from littlejohn.entrypoints.asgi import api
 
 
 @pytest.fixture
 def make_service():
-    def make() -> StockService:
-        return StockService()
+    def make(portfolio_repository: PortfolioRepository = None) -> StockService:
+        return StockService(
+            portfolio_repository=(
+                portfolio_repository
+                or PortfolioRepositoryDeterministicGenerator(portfolios={})
+            )
+        )
 
     return make
 
 
 @pytest.fixture
-def auth():
-    return (uuid4().hex, "")
+def make_auth():
+    def make():
+        return (uuid4().hex, "")
+
+    return make
 
 
 @pytest.fixture
@@ -49,24 +60,44 @@ def describe_get_portfolio():
         response = make_client().get("/tickers")
         assert 401 == response.status_code
 
-    def returns_unauthorized_if_username_is_not_uuid4(make_client, auth):
+    def returns_unauthorized_if_username_is_not_uuid4(make_client, make_auth):
+        auth = make_auth()
         username = "invalid"
         password = auth[1]
         response = make_client().get("/tickers", auth=(username, password))
         assert 401 == response.status_code
 
-    def returns_unauthorized_if_password_is_not_empty(make_client, auth):
+    def returns_unauthorized_if_password_is_not_empty(make_client, make_auth):
+        auth = make_auth()
         username = auth[0]
         password = "invalid"
         response = make_client().get("/tickers", auth=(username, password))
         assert 401 == response.status_code
 
-    def returns_portfolio_of_user(make_client, make_service, auth):
-        service = make_service()
-        expected_response_body = [
-            serialize_stock_price(stock_price)
-            for stock_price in service.get_portfolio()
-        ]
+    @pytest.mark.parametrize(
+        "portfolio",
+        [
+            pytest.param(["test1", "test2"], id="multiple stocks"),
+            pytest.param([], id="no stocks"),
+            pytest.param(["test"], id="single stock"),
+        ],
+    )
+    def returns_stocks_in_user_portfolio(
+        portfolio,
+        make_client,
+        make_service,
+        make_auth,
+    ):
+        auth = make_auth()
+        username = auth[0]
+
+        portfolio_repository = PortfolioRepositoryDeterministicGenerator(
+            portfolios={
+                username: portfolio,
+                "other-user": ["other1", "other2", "other3"],
+            },
+        )
+        service = make_service(portfolio_repository=portfolio_repository)
         response = make_client(service=service).get("/tickers", auth=auth)
         assert 200 == response.status_code
-        assert expected_response_body == response.json()
+        assert portfolio == [data["symbol"] for data in response.json()]
